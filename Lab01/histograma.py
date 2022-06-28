3ac47126e7b33dbdce8714d4c8ee4530ce4e644c
ERICK ECKERMANN CARDOSO <eec18@inf.ufpr.br>
2022-06-28 15:27:51 -0300

    Começando

---
+++ Lab01/histograma.py
@@ -0,0 +1,33 @@
+import cv2
+from matplotlib import pyplot as plt
+
+results_CORREL = list()
+results_CHISQR = list()
+results_INTERSECT = list()
+results_BHATTACHARYYA = list()
+
+for img in
+
+
+
+
+
+
+
+
+#img1 = cv2.imread('b2.bmp')
+#img2 = cv2.imread('b1.bmp')
+
+#hist1 = cv2.calcHist([img1], [2], None, [256], [0,256])
+#hist2 = cv2.calcHist([img2], [2], None, [256], [0,256])
+
+#sc = cv2.compareHist(hist1, hist2, cv2.HISTCMP_CORREL)
+#print(sc)
+
+#cv2.imshow("Display window", img1)
+#k = cv2.waitKey(0)
+
+#plt.plot (hist1, color = 'b')
+#plt.xlim ([0,256])
+#plt.show ()
+
