77d17d79cd1e60189751be4eb935088d52662553
ERICK ECKERMANN CARDOSO <eec18@inf.ufpr.br>
2022-07-07 15:14:42 -0300

    50%

--- Lab02/floresta.py
+++ Lab02/floresta.py
@@ -7,47 +7,26 @@ import matplotlib.pyplot as plt
 if len(sys.argv) != 3:
   print("ERRO DE SINTAXE: python3 floresta.py <IMG ENTRADA> <IMG SAIDA>")
   exit()
-img = cv2.imread(sys.argv[1])
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-limitador1 = (60, 200, 200)
-limitador2 = (80, 255, 100)
-mask = cv2.inRange(img_hsv, limitador1, limitador2)
+img = cv2.imread(sys.argv[1])
+img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+r, g, b = cv2.split(img_rgb)
+r, g, b = r.flatten(), g.flatten(), b.flatten()
+
+r_med = int(sum(r) / len(r))
+g_med = int(sum(g) / len(g))
+b_med = int(sum(b) / len(b))
+
+limitador1 = (r_med-40,g_med-40,b_med-40)
+limitador2 = (r_med+40,g_med+40,b_med+40)
+
+mask = cv2.inRange(img, limitador1, limitador2)
 result = cv2.bitwise_and(img, img, mask = mask)
 
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib import colors
-
-
-r, g, b = cv2.split(img)
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, 1, projection="3d")
-pixel_colors = img.reshape((np.shape(img)[0]*np.shape(img)[1], 3))
-norm = colors.Normalize(vmin=-1.,vmax=1.)
-norm.autoscale(pixel_colors)
-pixel_colors = norm(pixel_colors).tolist()
-axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
-axis.set_xlabel("Red")
-axis.set_ylabel("Green")
-axis.set_zlabel("Blue")
-plt.show()
-
-
-h, s, v = cv2.split(img_hsv)
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, 1, projection="3d")
-
-axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
-axis.set_xlabel("Hue")
-axis.set_ylabel("Saturation")
-axis.set_zlabel("Value")
-plt.show()
-
-"""
 plt.subplot(1, 2, 1)
-plt.imshow(mask, cmap="gray")
+plt.imshow(img)
 plt.subplot(1, 2, 2)
 plt.imshow(result)
 plt.show()
-"""
+
