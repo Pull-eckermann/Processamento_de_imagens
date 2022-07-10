1e01ef2983382bce652d1c80cdeeef7b6d756e0a
Pull-eckermann <eric7880@gmail.com>
2022-07-10 19:22:46 -0300

    Terminado

---
+++ Lab02/README.txt
@@ -0,0 +1,21 @@
+ERICK ECKERMANN CARDOSO
+GRR20186075
+
+Segundo trabalho de laboratório da disciplina de Processamento de imagens
+
+Código escrito em Python3, utilizando as seguintes bibliotecas:
+- Open-cv (cv2)
+- sys
+- numpy
+
+O código está contido inteiramente no arquivo floresta.py
+
+Antes de executar, certifique que o arquivo das imagens estejam no mesmo diretório que floresta.py, pois o código lê as imagens à partir do diretório corrente.
+
+Para compilar e executar o programa, basta utilizar o comando python3 com o nome do arquivo, seguido pela imagem a ser segmentada e o nome da imagem de saída, da seguinte maneira:
+
+$ python3 floresta.py <IMG ENTRADA> <IMG SAIDA>
+
+ex: python3 floresta.py f1.png out.png
+
+O programa criará uma imagem com o nome expecificado, sendo essa a imagem segmentada com as áreas de floresta.
--- Lab02/floresta.py
+++ Lab02/floresta.py
@@ -1,7 +1,6 @@
 import cv2
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
 
 #Confere se o programa foi executado com o número certo de argumentos
 if len(sys.argv) != 3:
@@ -9,21 +8,20 @@ if len(sys.argv) != 3:
   exit(0)
 
 img = cv2.imread(sys.argv[1])
-img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)       #Converte de imagem BGR para HSV
 
-h, s, v = cv2.split(img_hsv)
-h = h.flatten()
+h, s, v = cv2.split(img_hsv)                        #Separa os 3 canais e transforma em array
+h, s, v = h.flatten(), s.flatten(), v.flatten()
 
-h_med = int(sum(h) / len(h))
+h_med = int(sum(h) / len(h))                        #Calcula média do canal hue, para definir a cor dominante
 
-limitador1 = (h_med-30,0,0)
-limitador2 = (h_med+30,255,255)
+#Canal hue limitado entre +30 e -30 e demais pelo max e mínimo
+limitador1 = (h_med-30, int(min(s)), int(min(v)))
+limitador2 = (h_med+30, int(max(s)), int(max(v)))
 
 mask = cv2.inRange(img_hsv, limitador1, limitador2)
-result = cv2.bitwise_and(img, img, mask = mask)
+result = cv2.bitwise_and(img, img, mask = mask)     #And bit a bit aplicando a máscara
 
-plt.subplot(1, 2, 1)
-plt.imshow(img_hsv)
-plt.subplot(1, 2, 2)
-plt.imshow(result)
-plt.show()
+#Escreve o resultado no arquivo especificado na entrada
+cv2.imwrite(sys.argv[2], result)
+
