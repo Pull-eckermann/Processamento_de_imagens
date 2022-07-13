97fd4c5580282442d19c30785c65139bb9f7b3be
ERICK ECKERMANN CARDOSO <eec18@inf.ufpr.br>
2022-07-13 17:40:14 -0300

    90%

--- Lab03/filtro.py
+++ Lab03/filtro.py
@@ -1,4 +1,6 @@
+from multiprocessing.connection import wait
 from statistics import median
+from cv2 import waitKey
 import numpy as np
 import random
 import cv2
@@ -20,7 +22,7 @@ def sp_noise(image, prob):
 
 def filtro_media(img, mask):
     noise_img = sp_noise(img,float(sys.argv[2]))
-    media = cv2.GaussianBlur(noise_img,(mask),0)
+    media = cv2.GaussianBlur(noise_img,(mask,mask),0)
     return media
 
 def filtro_mediana(img, mask):
@@ -29,17 +31,20 @@ def filtro_mediana(img, mask):
     return mediana
 
 def filtro_empilhamento(img, nivel, n):
-    noise_img = list()
-    for i in range(0,n):
-        noise_img[i] = sp_noise(img, nivel)
+    img_emp = sp_noise(img, nivel)
+    for i in range(0,n-1):
+        noise_img = sp_noise(img, nivel)
+        img_emp = cv2.add(noise_img,img_emp)
+    img_emp = img_emp/n
+    return img_emp
 
 #Programa principal
 if len(sys.argv) != 5:
     print("ERRO DE SINTAXE: python3 filtro.py <IMG ORIGINAL> <NIVEL RUIDO> <FILTRO> <IMG SAIDA")
     exit(0)
 
-mask_media = 5
-mask_mediana = 5
+mask_media = 10
+mask_mediana = 10
 n_empilhamento = 6
 img = cv2.imread(sys.argv[1],0)
 
@@ -51,4 +56,7 @@ if sys.argv[3] == '1':
 
 if sys.argv[3] == '2':
     filter_img = filtro_empilhamento(img, float(sys.argv[2]), n_empilhamento)
+
+print("PSNR comparation:", cv2.PSNR(img, filter_img))
+cv2.imwrite(sys.argv[4], filter_img)
 
