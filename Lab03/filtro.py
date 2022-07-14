d1a94ecdc537a8882cce7264b5d0edb0e5b8fffa
Pull-eckermann <eric7880@gmail.com>
2022-07-14 00:04:30 -0300

    99%

--- Lab03/filtro.py
+++ Lab03/filtro.py
@@ -1,11 +1,11 @@
 from multiprocessing.connection import wait
 from statistics import median
-from cv2 import waitKey
 import numpy as np
 import random
 import cv2
 import sys
 
+#Função que adiciona ruído aleatório à imagem
 def sp_noise(image, prob):
     output = np.zeros(image.shape,np.uint8)
     thres = 1 - prob 
@@ -20,31 +20,36 @@ def sp_noise(image, prob):
                 output[i][j] = image[i][j]
     return output
 
+#FIltragem pela média
 def filtro_media(img, mask):
-    noise_img = sp_noise(img,float(sys.argv[2]))
-    media = cv2.GaussianBlur(noise_img,(mask,mask),0)
+    noise_img = sp_noise(img,float(sys.argv[2]))        #Gera imagem ruidosa
+    media = cv2.GaussianBlur(noise_img,(mask,mask),0)   #Aplica filtro por média
     return media
 
 def filtro_mediana(img, mask):
-    noise_img = sp_noise(img,float(sys.argv[2]))
-    mediana = cv2.medianBlur(noise_img,mask)
+    noise_img = sp_noise(img,float(sys.argv[2]))        #Gera imagem ruidosa
+    mediana = cv2.medianBlur(noise_img,mask)            #Aplica filtro mediana
     return mediana
 
 def filtro_empilhamento(img, nivel, n):
     img_emp = sp_noise(img, nivel)
+    #Gera n imagens e soma pixel a pixel, guardando em img_emp  
     for i in range(0,n-1):
         noise_img = sp_noise(img, nivel)
         img_emp = cv2.add(noise_img,img_emp)
-    img_emp = img_emp/n
+    
+    #Divide pelo número de imagens empilhadas, gerando a média
+    img_emp = cv2.divide(img_emp, n)
+
     return img_emp
 
 #Programa principal
 if len(sys.argv) != 5:
-    print("ERRO DE SINTAXE: python3 filtro.py <IMG ORIGINAL> <NIVEL RUIDO> <FILTRO> <IMG SAIDA")
+    print("ERRO DE SINTAXE: python3 filtro.py <IMG ORIGINAL> <NIVEL RUIDO> <FILTRO> <IMG SAIDA>")
     exit(0)
 
-mask_media = 10
-mask_mediana = 10
+mask_media = 15
+mask_mediana = 5
 n_empilhamento = 6
 img = cv2.imread(sys.argv[1],0)
 
