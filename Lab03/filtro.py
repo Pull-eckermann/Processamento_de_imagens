3bdcb5233fa10f05bc503389f0bf8da0e5ff17e8
Pull-eckermann <eric7880@gmail.com>
2022-07-17 23:16:33 -0300

    aL3 finalizado

--- Lab03/filtro.py
+++ Lab03/filtro.py
@@ -26,19 +26,21 @@ def filtro_media(img, mask):
     media = cv2.GaussianBlur(noise_img,(mask,mask),0)   #Aplica filtro por média
     return media
 
+#FIltragem pela mediana
 def filtro_mediana(img, mask):
     noise_img = sp_noise(img,float(sys.argv[2]))        #Gera imagem ruidosa
     mediana = cv2.medianBlur(noise_img,mask)            #Aplica filtro mediana
     return mediana
 
+#Filtro de empilhamento de imagens
 def filtro_empilhamento(img, nivel, n):
     img_emp = sp_noise(img, nivel)
     img_emp = cv2.divide(img_emp, n)
     #Gera n imagens e soma pixel a pixel, guardando em img_emp  
     for i in range(0,n-1):
         noise_img = sp_noise(img, nivel)
-        noise_img = cv2.divide(noise_img, n)
-        img_emp = cv2.add(noise_img,img_emp)
+        noise_img = cv2.divide(noise_img, n)            #Divide pelo número de imagens e soma
+        img_emp = cv2.add(noise_img,img_emp)            #a fim de evitar overflow
 
     return img_emp
 
@@ -47,9 +49,9 @@ if len(sys.argv) != 5:
     print("ERRO DE SINTAXE: python3 filtro.py <IMG ORIGINAL> <NIVEL RUIDO> <FILTRO> <IMG SAIDA>")
     exit(0)
 
-mask_media = 15
-mask_mediana = 5
-n_empilhamento = 6
+mask_media = 13                 #Tamanho da máscara definida para media
+mask_mediana = 5                    #Mascara para mediana
+n_empilhamento = 20                #Número de imagens a empilhar
 img = cv2.imread(sys.argv[1],0)
 
 if sys.argv[3] == '0':
@@ -61,6 +63,6 @@ if sys.argv[3] == '1':
 if sys.argv[3] == '2':
     filter_img = filtro_empilhamento(img, float(sys.argv[2]), n_empilhamento)
 
-print("PSNR comparation:", cv2.PSNR(img, filter_img))
+print("PSNR comparation:", cv2.PSNR(img, filter_img))   #Taxa de semelhança entre imagem original e filtrada
 cv2.imwrite(sys.argv[4], filter_img)
 
