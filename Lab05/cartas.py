3657a156eab6a191d51afc035be28062fff77413
Erick Eckermann <44233963+Pull-eckermann@users.noreply.github.com>
2022-08-09 11:31:20 -0300

    Update cartas.py

--- Lab05/cartas.py
+++ Lab05/cartas.py
@@ -11,20 +11,20 @@ if len(sys.argv) != 2:
     exit(0)
 
 if sys.argv[1] == '-l':
-  for aux in os.listdir('./tr/'):       #Itera sobre as imagens do diretório especificado
+  for aux in os.listdir('./tr/'):      #Itera sobre as imagens do diretório especificado
     if not(aux.endswith(".jpg")):      #Se não for imagem.jpg pula pra próxima iteração
       continue
     img = cv2.imread('./tr/'+aux, 0)
     _, bin_img = cv2.threshold(img,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    bin_img = cv2.medianBlur(bin_img,5)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+    bin_img = cv2.dilate(bin_img,kernel,iterations = 20)
+    bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel)
   
-    bin_media = cv2.GaussianBlur(bin_img, (21,21), 0)
-    kernel = np.ones((5,5),np.uint8)
-    bin_img = cv2.dilate(bin_img,kernel,iterations = 3)
-    #bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_OPEN, kernel)
+    cnt,_ = cv2.findContours(bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(cnt))
 
     plt.imshow(bin_img, 'gray')
     plt.title('Input Image'), plt.xticks([]), plt.yticks([])
     plt.show()
-
-
-
