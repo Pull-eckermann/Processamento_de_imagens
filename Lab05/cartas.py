0993366ae50662561b466b74c30ebb866525b7b7
Erick Eckermann <44233963+Pull-eckermann@users.noreply.github.com>
2022-08-17 12:08:41 -0300

    Update cartas.py

--- Lab05/cartas.py
+++ Lab05/cartas.py
@@ -1,3 +1,4 @@
+from email.mime import image
 import numpy as np
 import cv2
 import sys
@@ -16,33 +17,41 @@ if sys.argv[1] == '-l':
       continue
     img = cv2.imread(aux, 0)
     
-    _, bin_img = cv2.threshold(img,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    _, bin_img = cv2.threshold(img,0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
     bin_img = cv2.medianBlur(bin_img,5)
-    kernel = np.array([[0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0],
-                       [1, 1, 1, 1, 1],
-                       [0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0]], dtype=np.uint8)
-    h_dilate = cv2.dilate(bin_img,kernel,iterations = 20)
-    kernel = np.array([[0, 0, 1, 0, 0],
-                       [0, 0, 1, 0, 0],
-                       [0, 0, 1, 0, 0],
-                       [0, 0, 1, 0, 0],
-                       [0, 0, 1, 0, 0]], dtype=np.uint8)
-    v_dilate = cv2.dilate(bin_img,kernel,iterations = 10)
-    bin_img = cv2.bitwise_and(h_dilate,v_dilate)
-    
-    kernel = np.array([[0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0],
-                       [1, 1, 1, 1, 1],
-                       [0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0]], dtype=np.uint8)
-    #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-    bin_img = cv2.dilate(bin_img,kernel,iterations = 20)
-    bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_OPEN, kernel)
-    bin_img = cv2.medianBlur(bin_img,15)
+#    kernel = np.array([[0, 0, 0, 0, 0],
+#                       [0, 0, 0, 0, 0],
+#                       [1, 1, 1, 1, 1],
+#                       [0, 0, 0, 0, 0],
+#                       [0, 0, 0, 0, 0]], dtype=np.uint8)
+#    h_dilate = cv2.dilate(bin_img,kernel,iterations = 20)
+#    kernel = np.array([[0, 0, 1, 0, 0],
+#                       [0, 0, 1, 0, 0],
+#                       [0, 0, 1, 0, 0],
+#                       [0, 0, 1, 0, 0],
+#                       [0, 0, 1, 0, 0]], dtype=np.uint8)
+#    v_dilate = cv2.dilate(bin_img,kernel,iterations = 10)
+#    bin_img = cv2.bitwise_and(h_dilate,v_dilate)
+#    
+#    kernel = np.array([[0, 0, 0, 0, 0],
+#                       [0, 0, 0, 0, 0],
+#                       [1, 1, 1, 1, 1],
+#                       [0, 0, 0, 0, 0],
+#                       [0, 0, 0, 0, 0]], dtype=np.uint8)
+#    bin_img = cv2.dilate(bin_img,kernel,iterations = 20)
+    #Tentativa de rotação automática https://pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
+    coords = np.column_stack(np.where(bin_img > 0))
+    angle = cv2.minAreaRect(coords)[-1]
 
-    #cv2.line(bin_img,(x1,y1),(x2,y2),(0,255,0),2)
+    (h, w) = bin_img.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    bin_img = cv2.warpAffine(bin_img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    print(angle)
+
+    plt.imshow(bin_img, 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
 
     cont = 0  
     cnt,_ = cv2.findContours(bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,6 +65,3 @@ if sys.argv[1] == '-l':
       acertos = acertos + 1
 
 print('Cartas corretas:',acertos,'de 10')
-#plt.imshow(bin_img, 'gray')
-#plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-#plt.show()
