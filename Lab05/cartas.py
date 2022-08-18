4ade468a9cdd5800216711a7603344217a290591
Erick Eckermann <44233963+Pull-eckermann@users.noreply.github.com>
2022-08-18 12:07:18 -0300

    Update cartas.py

--- Lab05/cartas.py
+++ Lab05/cartas.py
@@ -1,4 +1,3 @@
-from email.mime import image
 import numpy as np
 import cv2
 import sys
@@ -19,35 +18,65 @@ if sys.argv[1] == '-l':
     
     _, bin_img = cv2.threshold(img,0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
     bin_img = cv2.medianBlur(bin_img,5)
-#    kernel = np.array([[0, 0, 0, 0, 0],
-#                       [0, 0, 0, 0, 0],
-#                       [1, 1, 1, 1, 1],
-#                       [0, 0, 0, 0, 0],
-#                       [0, 0, 0, 0, 0]], dtype=np.uint8)
-#    h_dilate = cv2.dilate(bin_img,kernel,iterations = 20)
-#    kernel = np.array([[0, 0, 1, 0, 0],
-#                       [0, 0, 1, 0, 0],
-#                       [0, 0, 1, 0, 0],
-#                       [0, 0, 1, 0, 0],
-#                       [0, 0, 1, 0, 0]], dtype=np.uint8)
-#    v_dilate = cv2.dilate(bin_img,kernel,iterations = 10)
-#    bin_img = cv2.bitwise_and(h_dilate,v_dilate)
-#    
-#    kernel = np.array([[0, 0, 0, 0, 0],
-#                       [0, 0, 0, 0, 0],
-#                       [1, 1, 1, 1, 1],
-#                       [0, 0, 0, 0, 0],
-#                       [0, 0, 0, 0, 0]], dtype=np.uint8)
-#    bin_img = cv2.dilate(bin_img,kernel,iterations = 20)
-    #Tentativa de rotação automática https://pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
-    coords = np.column_stack(np.where(bin_img > 0))
-    angle = cv2.minAreaRect(coords)[-1]
 
-    (h, w) = bin_img.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    bin_img = cv2.warpAffine(bin_img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    #Tentativa de rotação automática https://pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
+    coords = list()
+    h, l = bin_img.shape
+    for i in range(1,h):
+      for x in range(1,l):
+        if bin_img(h,l) == 255:
+          coords.append((h,l))
+          break
+      break
+    for i in range(h,1):
+      for x in range(1,l):
+        if bin_img[h,l] == 255:
+          coords.append((h,l))
+          break
+      break
+    for i in range(1,h):
+      for x in range(l,1):
+        if bin_img[h,l] == 255:
+          coords.append((h,l))
+          break
+      break
+    for i in range(h,l):
+      for x in range(l,1):
+        if bin_img[h,l] == 255:
+          coords.append((h,l))
+          break
+      break
+    
+    
+    #coords = np.column_stack(np.where(bin_img > 0))
+    _, _ ,angle = cv2.minAreaRect(coords)
+    
+    if angle < 45:
+      (h, w) = bin_img.shape[:2]
+      center = (w // 2, h // 2)
+      M = cv2.getRotationMatrix2D(center, angle, 1)
+      bin_img = cv2.warpAffine(bin_img, M, (w, h))
     print(angle)
+
+    # kernel = np.array([[0, 0, 0, 0, 0],
+    #                   [0, 0, 0, 0, 0],
+    #                   [1, 1, 1, 1, 1],
+    #                   [0, 0, 0, 0, 0],
+    #                   [0, 0, 0, 0, 0]], dtype=np.uint8)
+    #h_dilate = cv2.dilate(bin_img,kernel,iterations = 20)
+    #kernel = np.array([[0, 0, 1, 0, 0],
+    #                   [0, 0, 1, 0, 0],
+    #                   [0, 0, 1, 0, 0],
+    #                   [0, 0, 1, 0, 0],
+    #                   [0, 0, 1, 0, 0]], dtype=np.uint8)
+    #v_dilate = cv2.dilate(bin_img,kernel,iterations = 10)
+    #bin_img = cv2.bitwise_and(h_dilate,v_dilate)
+    bin_img = cv2.medianBlur(bin_img,5)
+    
+    kernel = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=np.uint8)
+    bin_img = cv2.dilate(bin_img,kernel,iterations = 20)
 
     plt.imshow(bin_img, 'gray')
     plt.title('Input Image'), plt.xticks([]), plt.yticks([])
